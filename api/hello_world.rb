1178412281011ca49a748b441e3f79635d625824
Matt Aimonetti <mattaimonetti@gmail.com>
2012-02-06 12:08:53 -0800

    Update api/hello_world.rb

--- api/hello_world.rb
+++ api/hello_world.rb
@@ -3,8 +3,10 @@ describe_service "hello_world" do |service|
   service.http_verb :get
   service.disable_auth # on by default
 
+  # INPUT
   service.param.string  :name, :default => 'World'
 
+  # OUTPUT
   service.response do |response|
     response.object do |obj|
 	    obj.string :message, :doc => "The greeting message sent back. Defaults to 'World'"
@@ -12,12 +14,14 @@ describe_service "hello_world" do |service|
 	  end
   end
 
+  # DOCUMENTATION
   service.documentation do |doc|
   	doc.overall "This service provides a simple hello world implementation example."
   	doc.param :name, "The name of the person to greet."
   	doc.example "<code>http://localhost:9292/hello_world?name=Matt</code>"
  end
 
+  # ACTION/IMPLEMENTATION
   service.implementation do
     {:message => "Hello #{params[:name]}", :at => Time.now}.to_json
   end
