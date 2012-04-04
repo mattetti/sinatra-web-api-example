72fb24bb7f2d0ab5aa5a1eb2a99ae8235f48decc
Dr Nic Williams <drnicwilliams@gmail.com>
2012-04-04 13:41:26 -0700

    Separate out the assert_api_response test from the response value tests

--- test/api/hello_world_test.rb
+++ test/api/hello_world_test.rb
@@ -3,18 +3,19 @@ require 'test_helpers'
 
 class HelloWorldTest < MiniTest::Unit::TestCase
 
+  def test_response
+    TestApi.get "/hello_world"
+    assert_api_response
+  end
+
   def test_default_response
     TestApi.get "/hello_world"
-    response = TestApi.json_response
-    assert_api_response response
-    assert_equal response["message"], "Hello World"
+    assert_equal TestApi.json_response["message"], "Hello World"
   end
 
   def test_override_response
     TestApi.get "/hello_world", :name => 'Matt'
-    response = TestApi.json_response
-    assert_api_response response
-    assert_equal response["message"], "Hello Matt"
+    assert_equal TestApi.json_response["message"], "Hello Matt"
   end
 
 end
