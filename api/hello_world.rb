64ffa120f1fdf4d11a9207490e44784a31dbd3d7
Justin Mazzi <hh@mailheist.com>
2012-04-04 14:59:37 -0300

    Fix indentation.

--- api/hello_world.rb
+++ api/hello_world.rb
@@ -9,17 +9,17 @@ describe_service "hello_world" do |service|
   # OUTPUT
   service.response do |response|
     response.object do |obj|
-	    obj.string :message, :doc => "The greeting message sent back. Defaults to 'World'"
+      obj.string :message, :doc => "The greeting message sent back. Defaults to 'World'"
       obj.datetime :at, :doc => "The timestamp of when the message was dispatched"
-	  end
+    end
   end
 
   # DOCUMENTATION
   service.documentation do |doc|
-  	doc.overall "This service provides a simple hello world implementation example."
-  	doc.param :name, "The name of the person to greet."
-  	doc.example "<code>curl -I 'http://localhost:9292/hello_world?name=Matt'</code>"
- end
+    doc.overall "This service provides a simple hello world implementation example."
+    doc.param :name, "The name of the person to greet."
+    doc.example "<code>curl -I 'http://localhost:9292/hello_world?name=Matt'</code>"
+  end
 
   # ACTION/IMPLEMENTATION
   service.implementation do
